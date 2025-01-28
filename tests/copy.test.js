4b1e379072793700d8a3511b2f38ed5c744eca77
Shaun Chong <data-miner00@users.noreply.github.com>
2025-01-28 19:21:47 +0800

    test: Add files to test

--- tests/copy.test.js
+++ tests/copy.test.js
@@ -2,7 +2,14 @@ var assert = require("node:assert");
 var { test } = require("node:test");
 var { existsSync } = require("node:fs");
 
-["sitemap.xsl", "sitemap.xml", "index.html", "tailwind.css"].forEach((file) => {
+[
+  "sitemap.xsl",
+  "sitemap.xml",
+  "index.html",
+  "tailwind.min.css",
+  "css-encoded.txt",
+  "xsl-encoded.txt",
+].forEach((file) => {
   test(`${file} found in dist folder`, () => {
     assert.equal(existsSync(`dist/${file}`), true);
   });
