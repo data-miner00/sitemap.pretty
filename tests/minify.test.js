bccdd7d71323a6d07b8cc97a01b1e45c1aa962d7
Shaun Chong <data-miner00@users.noreply.github.com>
2025-02-10 21:35:35 +0800

    test: Remove test for css minify

--- tests/minify.test.js
+++ tests/minify.test.js
@@ -2,12 +2,6 @@ var assert = require("node:assert");
 var { test } = require("node:test");
 var { statSync } = require("node:fs");
 
-test("minified css file is smaller than source file", () => {
-  var originalSize = statSync("dist/tailwind.css").size;
-  var minifiedSize = statSync("dist/tailwind.min.css").size;
-  assert.ok(minifiedSize < originalSize);
-});
-
 test("minified xsl file is smaller than source file", () => {
   var originalSize = statSync("dist/sitemap.xsl").size;
 
