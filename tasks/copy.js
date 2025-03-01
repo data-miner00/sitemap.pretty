9b5cc083d16939349a724ae821078c661f93c5b0
Shaun Chong <data-miner00@users.noreply.github.com>
2025-03-01 17:47:56 +0800

    chore: Separate copy task to include rename

--- tasks/copy.js
+++ tasks/copy.js
@@ -1,3 +1,5 @@
+var path = require("node:path");
+
 /**
  * The task for copying files.
  * Refer to https://github.com/gruntjs/grunt-contrib-copy
@@ -10,16 +12,44 @@ module.exports = function (grunt) {
     default: {
       expand: true,
       cwd: "<%= srcPath %>",
-      src: [
-        "index.html",
-        "sitemap.xml",
-        "sitemap.xsl",
-        "sitemapindex.xml",
-        "basic.xsl",
-      ],
+      src: ["index.html"],
       dest: "<%= distPath %>",
+    },
+    vogue: {
+      expand: true,
+      cwd: "<%= srcPath %>",
+      src: ["sitemap.xml", "sitemapindex.xml", "sitemap.xsl"],
+      dest: "<%= distPath %>",
+      rename: function (src, dest) {
+        return renameAux(src, dest, "vogue");
+      },
+    },
+    basic: {
+      expand: true,
+      cwd: "<%= srcPath %>",
+      src: ["sitemap.xml", "sitemapindex.xml", "basic.xsl"],
+      dest: "<%= distPath %>",
+      rename: function (src, dest) {
+        return renameAux(src, dest, "basic");
+      },
     },
   };
 
   grunt.config("copy", config);
+
+  function renameAux(src, dest, themeName) {
+    var [filename, extension] = dest.split(".");
+    var rename = filename + "-" + themeName + "." + extension;
+
+    if (extension === "xsl") {
+      var destination = path.join(src, dest);
+      return destination;
+    }
+
+    grunt.log.writeln(`Renamed ${dest} to ${rename}.`);
+
+    var destination = path.join(src, rename);
+
+    return destination;
+  }
 };
