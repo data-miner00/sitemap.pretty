04f2e651289210ce6e9cd11faa6acae4d71f412b
Shaun Chong <data-miner00@users.noreply.github.com>
2025-03-03 18:24:09 +0800

    chore: Transform xsl files only

--- tasks/base64.js
+++ tasks/base64.js
@@ -1,7 +1,6 @@
 var { existsSync } = require("node:fs");
 var { join } = require("node:path");
-var cssFile = "tailwind.min.css",
-  xslFile = "sitemap.xsl";
+var xslFiles = ["vogue.xsl", "basic.xsl"];
 var verbose = false;
 
 /**
@@ -10,20 +9,25 @@ var verbose = false;
  */
 module.exports = function (grunt) {
   grunt.registerTask("base64:xsl", function () {
-    if (!existsSync(join("dist", xslFile))) {
-      grunt.warn(`The file '${xslFile}' is not found.`);
-      return;
-    }
+    xslFiles.forEach((xslFile) => {
+      if (!existsSync(join("dist", xslFile))) {
+        grunt.warn(`The file '${xslFile}' is not found.`);
+        return;
+      }
 
-    var content = grunt.file.read(join("dist", xslFile), {
-      encoding: null,
+      var content = grunt.file.read(join("dist", xslFile), {
+        encoding: null,
+      });
+
+      var base64encoded = content.toString("base64");
+      var xslPadded = "data:text/xsl;base64," + base64encoded;
+
+      var encodeFileName = xslFile.split(".")[0] + "-encoded.txt";
+
+      grunt.file.write(join("dist", encodeFileName), xslPadded);
+      grunt.log.writeln(`Base 64 encoded ${xslFile}`);
+
+      if (verbose) grunt.log.writeln(`Content: ${xslPadded}`);
     });
-
-    var base64encoded = content.toString("base64");
-    var xslPadded = "data:text/xsl;base64," + base64encoded;
-
-    grunt.file.write(join("dist", "xsl-encoded.txt"), xslPadded);
-    grunt.log.writeln(`Base 64 encoded ${xslFile}`);
-    if (verbose) grunt.log.writeln(`Content: ${xslPadded}`);
   });
 };
